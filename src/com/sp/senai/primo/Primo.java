62fa8167f51a31845df8c487ba4e391280849734
Paulo19961944 <106405308+Paulo19961944@users.noreply.github.com>
2023-08-05 13:45:53 -0300

    Update Primo.java

--- src/com/sp/senai/primo/Primo.java
+++ src/com/sp/senai/primo/Primo.java
@@ -9,23 +9,25 @@ public class Primo {
 
         /** Captura um numero */
         System.out.print("Digite um numero: ");
-        int numero = teclado.nextInt();
-        System.out.println();
-
-        /** Verifica se o numero é primo */
-        boolean primo = true;
-        if (numero <= 1) {
-            primo = false;
-        } else if (numero == 2) {
-            primo = true;
-        } else if (numero % 2 == 0) {
-            primo = false;
-        }
-        /** Printa na Tela */
-        if (primo) {
-            System.out.println("O numero e primo");
-        } else {
-            System.out.println("O Numero nao e primo");
+        if (teclado.hasNextInt()) {
+            int numero = teclado.nextInt();
+            System.out.println();
+            
+            /** Verifica se o numero é primo */
+            boolean primo = true;
+            if (numero <= 1) {
+                primo = false;
+            } else if (numero == 2) {
+                primo = true;
+            } else if (numero % 2 == 0) {
+                primo = false;
+            }
+            /** Printa na Tela */
+            if (primo) {
+                System.out.println("O numero e primo");
+            } else {
+                System.out.println("O Numero nao e primo");
+            }
         }
     }
 }
